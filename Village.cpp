45fdf37d212358246d278263290fc84935ef249a
0x506978656c <66031115+0x506978656c@users.noreply.github.com>
2022-06-21 18:24:31 +0200

    Update Village.cpp

--- Village.cpp
+++ Village.cpp
@@ -3,6 +3,51 @@
  *
  */
 
+
+
+
+
+/**
+ * @brief Checks if a iron golem can spawn within the boundary box by cycling though all the surface area positions in the 16x6x16 area
+ * 
+ * @param Village *
+ * @return boolean if a golem can spawn within the boundary box
+ */
+bool Village::canSpawnAt(Village * this, int a2, int a3, int a4, int a5, int a6, int a7) {
+
+  Dimension * dimension = Level::getDimension(this -> mLevel, VanillaDimensions::Overworld);
+  BlockSource * blockSource = Dimension::getBlockSourceDEPRECATEDUSEPLAYERREGIONINSTEAD(dimension);
+  Block * block01 = BlockSource::getBlock(blockSource, a2, a3 - 1, a4);
+  Block * block02;
+  if (!Block::isSolid(block01))
+    return 0;
+
+  bool bool1 = (a5 >= 1 ? (a6 < 1) : (a5 < 1))
+  if (!bool1) {
+    int v15 = a4 - a7 / 2;
+    int v16 = a2 - a5 / 2;
+    int v20 = v16 + a5;
+    int v21 = a3 + a6;
+
+    for (int i = v16; v18 = a3, i < v20; i++) {
+      for (; v17 = v15, v18 < v21; v18++) {
+        for (; v17 < v15 + a7; v17++) {
+          block02 = BlockSource::getBlock(blockSource, v16, v18, v17);
+          if (Block::isSolid(block02))
+            return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+bool Village::canSpawnAtBlockPos(Village *this, const BlockPos *a2, const BlockPos *a3){ //in code it is just Village::canSpawnAt
+ 
+  return Village::canSpawnAt(this, *a2, *(a2 + 1), *(a2 + 2), *a3, *(a3 + 1), *(a3 + 2));
+
+}
+
 /**
  * @brief Counts the golems inside the boundry box 
  * 
